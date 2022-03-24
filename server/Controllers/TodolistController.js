b368da696fd43acdac2f963a0cbb609a9bd8db90
EmmieKr <48325116+EmmieKr@users.noreply.github.com>
2022-03-24 09:02:23 -0400

    Update TodolistController.js

--- server/Controllers/TodolistController.js
+++ server/Controllers/TodolistController.js
@@ -1,102 +1,101 @@
-import { log } from 'console';
-import { todolist as _todolist } from '../models';
-import Todolist from '../Models/Todolist';
+const console = require('console')
+const models = require('../models')
+const Todolist = require('../Models/Todolist')
 
-export async function create(req, res) {
-    log(req);
-    if (!req.body) {
-        res.status(400).send({
-            message: "Content can not be empty!"
-        });
-        return;
-    }
-    log(req);
-    const todo = {
-        name: req.body.name,
-        processTodo: req.body.processTodo,
-        processDoing: req.body.processDoing,
-        processDone: req.body.processDone
-    };
-    _todolist.create(todo)
-
-        .then(data => {
-            res.send(data);
-        })
-        .catch(err => {
-            res.status(500).send({
-                message: err.message || "Some error occurred while creating the todo."
+module.exports={
+    async create(req, res) {
+        console.log(req);
+        if (!req.body) {
+            res.status(400).send({
+                message: "Content can not be empty!"
             });
-        });
-}
-export async function getAll(req, res) {
-    let objectArray = [];
-    let object = {
-        id: null,
-        name: "",
-        processTodo: false,
-        processDoing: false,
-        processDone: false
-    };
-    try {
-        const todolist = await _todolist.findAll({
-            raw: true
-        })
+            return;
+        }
+        console.log(req);
+        const todo = {
+            name: req.body.name,
+            processTodo: req.body.processTodo,
+            processDoing: req.body.processDoing,
+            processDone: req.body.processDone
+        };
+        models.todolist.create(todo)
+    
             .then(data => {
                 res.send(data);
+            })
+            .catch(err => {
+                res.status(500).send({
+                    message: err.message || "Some error occurred while creating the todo."
+                });
             });
-    } catch (err) {
-        log(err);
-        res.status(400).send({
-            error: "Some error occurred while retrieving todolists."
-        });
+    },
+     async getAll(req, res) {
+        let objectArray = [];
+        let object = {
+            id: null,
+            name: "",
+            processTodo: false,
+            processDoing: false,
+            processDone: false
+        };
+        try {
+            const todolist = await models.todolist.findAll({
+                raw: true
+            })
+                .then(data => {
+                    res.send(data);
+                });
+        } catch (err) {
+            console.log(err);
+            res.status(400).send({
+                error: "Some error occurred while retrieving todolists."
+            });
+        }
+    },
+        async update(req, res) {
+        const id = req.params.id;
+        models.todolist.update(req.body, {
+            where: { id: id }
+        })
+            .then(num => {
+                if (num == 1) {
+                    res.send({
+                        message: 'Todo was updated successfully.'
+                    });
+                } else {
+                    res.send({
+                        message: `Cannot update todo with id=${id}. Maybe todo was not
+                        found or is empty!`
+                    });
+                }
+            })
+            .catch(err => {
+                res.status(500).send({
+                    message: `Error updating Todo with id=${id}`
+                });
+            });
+    },
+        async deleteId(req, res) {
+        const id = req.params.id;
+        models.todolist.destroy({
+            where: { id: id }
+        })
+            .then(num => {
+                if (num == 1) {
+                    res.send({
+                        message: "Todo was deleted successfully!"
+                    });
+                } else {
+                    res.send({
+                        message: `Cannot delete todo with id=${id}. Product not found!`
+                    });
+                }
+            })
+            .catch(err => {
+                res.status(500).send({
+                    message: `Could not delete Todo with id=${id}`
+                });
+            });
     }
 }
-export
-    //update
-    async function update(req, res) {
-    const id = req.params.id;
-    _todolist.update(req.body, {
-        where: { id: id }
-    })
-        .then(num => {
-            if (num == 1) {
-                res.send({
-                    message: 'Todo was updated successfully.'
-                });
-            } else {
-                res.send({
-                    message: `Cannot update todo with id=${id}. Maybe todo was not
-                    found or is empty!`
-                });
-            }
-        })
-        .catch(err => {
-            res.status(500).send({
-                message: `Error updating Todo with id=${id}`
-            });
-        });
-}
-export
-    //delete
-    async function deleteId(req, res) {
-    const id = req.params.id;
-    _todolist.destroy({
-        where: { id: id }
-    })
-        .then(num => {
-            if (num == 1) {
-                res.send({
-                    message: "Todo was deleted successfully!"
-                });
-            } else {
-                res.send({
-                    message: `Cannot delete todo with id=${id}. Product not found!`
-                });
-            }
-        })
-        .catch(err => {
-            res.status(500).send({
-                message: `Could not delete Todo with id=${id}`
-            });
-        });
-}
+ 
